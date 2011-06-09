f45234fa5f5ca0b3f251b4cd685cd6cd581b5ece
Peter Row <peter.row@gmail.com>
2011-06-10 05:34:52 +1000

    adding wsgi

---
+++ passenger_wsgi.py
@@ -0,0 +1,15 @@
+import sys, os
+INTERP = os.path.expanduser("~/bin/python")
+if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
+
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+application = tornado.wsgi.WSGIApplication([
+        (r"/", MainHandler),
+    ])
+
