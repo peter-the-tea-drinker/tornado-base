401d04ea549abd1684814b4f30b2e421c0d62554
Peter Row <peter.row@gmail.com>
2011-06-11 07:28:34 +1000

    fixing fabfil

--- fabbase.py
+++ fabbase.py
@@ -1,18 +1,31 @@
-from fabric.api import local
+CODE_DIR = '~/example.com'
+REPO = 'git@github.com:peter-the-tea-drinker/tornado-base.git'
+TAG = ''
+
+from fabric.api import local, settings, abort, run, cd, env
+# dump this into the wsgi 
+
+settings_dict = dict()
+
+def update_settings(**updated_settings):
+    settings_dict.update(updated_settings)
+
+# set defaults for settings_dict
+update_settings(code_dir=CODE_DIR,repo=REPO,release_tag=TAG)
 
 def hello():
     print("Hello world!")
 
-def commit():
-    local("git add -p && git commit")
-
-
 def deploy():
-    code_dir = '/srv/django/myproject'
+    code_dir = settings_dict['code_dir']
+    repo = settings_dict['repo']
+    release_tag = settings_dict['release_tag']
     with settings(warn_only=True):
-        if run("test -d %s" % code_dir).failed:
-            run("git clone user@vcshost:/path/to/repo/.git %s" % code_dir)
+        if run("test -d %s/.git" % code_dir).failed:
+            run("git clone %s %s" % (repo,code_dir))
     with cd(code_dir):
         run("git pull")
-        run("touch app.wsgi")
+        if release_tag:
+            run('git checkout :/"%s"'%release_tag)
+        run("touch tmp/restart.txt")
 
