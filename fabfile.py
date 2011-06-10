6ba930693700d51f8edf4943fffe31fe4fdae5ef
Peter Row <peter.row@gmail.com>
2011-06-10 23:45:40 +1000

    adding fabfile

---
+++ fabfile.py
@@ -0,0 +1,18 @@
+from fabric.api import local
+
+def hello():
+    print("Hello world!")
+
+def commit():
+    local("git add -p && git commit")
+
+
+def deploy():
+    code_dir = '/srv/django/myproject'
+    with settings(warn_only=True):
+        if run("test -d %s" % code_dir).failed:
+            run("git clone user@vcshost:/path/to/repo/.git %s" % code_dir)
+    with cd(code_dir):
+        run("git pull")
+        run("touch app.wsgi")
+
