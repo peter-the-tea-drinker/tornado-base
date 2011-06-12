1bfdea5475ea2b6b90a78ded6d0d6db2e6f3c390
Peter Row <peter.row@gmail.com>
2011-06-13 00:37:45 +1000

    moved stuff to another repo

--- example_fabfile.py
+++
@@ -1,9 +0,0 @@
-from fabbase import *
-
-CODE_DIR = '~/example.com'
-BASE_REPO = 'git@github.com:peter-the-tea-drinker/tornado-base.git'
-TAG = ''
-
-update_settings(code_dir=CODE_DIR,repo=REPO,release_tag=TAG)
-
-
--- passenger_wsgi.py
+++
@@ -1,4 +0,0 @@
-import sys,os.path
-sys.path.append(os.path.expanduser('~/repos/tornado-base'))
-from base.passenger_wsgi_base import *
-
